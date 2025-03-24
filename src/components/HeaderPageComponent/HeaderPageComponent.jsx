ae95def662cc13596c370f6590c8b0e79cbea238
AndriyBilous <andriy.bilous.92@gmail.com>
2025-03-24 13:37:54 +0200

    Make adjustments for contacts and logo

--- src/components/HeaderPageComponent/HeaderPageComponent.jsx
+++ src/components/HeaderPageComponent/HeaderPageComponent.jsx
@@ -1,8 +1,12 @@
 import "./HeaderPageComponent.css";
-import logo from "../../assets/logo.svg";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useRef } from "react";
+
+import logo from "../../assets/logo.svg";
+import viber from "../../assets/viber.svg";
+import telephone from "../../assets/telephone.svg";
+import mail from "../../assets/mail.svg";
 
 function HeaderPageComponent() {
   const [navigationHover, setNavigationHover] = useState("");
@@ -27,19 +31,25 @@ function HeaderPageComponent() {
       </div>
       <div className="right-side__container">
         <div className="contacts__container">
-          <div className="contacts__item">
-            <a href="mailto:mail.example.com">
-              <i className="fa-solid fa-envelope-open-text"></i>mail.example.com
+          <div className="contacts__item__container">
+            <a href="mailto:mail.example.com" className="contacts__item">
+              {/* <i className="fa-solid fa-envelope-open-text"></i> */}
+              <img src={mail} alt="mail_logo" className="contacts_icon" />
+              <p>mail.example.com</p>
             </a>
           </div>
-          <div className="contacts__item">
-            <a href="viber://chat?number=%2B4">
-              <i className=""></i>Viber
+          <div className="contacts__item__container">
+            <a href="viber://chat?number=%2B4" className="contacts__item">
+              {/* <i className=""></i> */}
+              <img src={viber} alt="viber_logo" className="contacts_icon" />
+              <p>Viber</p>
             </a>
           </div>
-          <div className="contacts__item">
-            <a href="tel:+380503333333">
-              <i className=""></i>+38(050) 333 33 33
+          <div className="contacts__item__container">
+            <a href="tel:+380503333333" className="contacts__item">
+              {/* <i className=""></i> */}
+              <img src={telephone} alt="telephone_logo" />
+              <p>+38(050) 333 33 33</p>
             </a>
           </div>
         </div>

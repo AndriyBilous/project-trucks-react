b1551a9d23434d9d9941c6c98f48ef467a0cfc89
AndriyBilous <andriy.bilous.92@gmail.com>
2025-03-31 16:10:01 +0300

    Add navigationBar background styles and tracking system

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,54 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
-import HeaderPageComponent from './components/HeaderPageComponent/HeaderPageComponent'
-import InfoPageComponent from './components/InfoPageComponent/InfoPageComponent'
-import FooterPageComponent from './components/FooterPageComponent/FooterPageComponent'
+import HeaderPageComponent from "./components/HeaderPageComponent/HeaderPageComponent";
+import InfoPageComponent from "./components/InfoPageComponent/InfoPageComponent";
+import FooterPageComponent from "./components/FooterPageComponent/FooterPageComponent";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [navBackgroundVisibility, setNavBackgroundVisibility] = useState(false);
+  const [navBarVisibility, setNavBarVisibility] = useState(false);
+
+  const navBackgroundRef = useRef();
+
+  useEffect(() => {
+    if (!navBackgroundVisibility) {
+      navBackgroundRef.current.style.display = "none";
+      // setNavBarVisibility(false);
+      console.log("background1: " + navBackgroundVisibility);
+      console.log("navBarVisibility: " + navBarVisibility);
+    } else {
+      navBackgroundRef.current.style.display = "flex";
+      console.log("background2: " + navBackgroundVisibility);
+    }
+  }, [navBackgroundVisibility]);
+
+  const handleMouseOnClickNavBarBackground = () => {
+    setNavBackgroundVisibility(() => !navBackgroundVisibility);
+
+    if (navBackgroundVisibility === true) {
+      setNavBarVisibility(false);
+      console.log("click on background: " + navBarVisibility);
+    }
+  };
 
   return (
-    <div className='main__container'>
-      <HeaderPageComponent/>
-      <InfoPageComponent/>
-      <FooterPageComponent/>
+    <div className="main__container">
+      <div
+        className="navigation__background"
+        ref={navBackgroundRef}
+        onClick={handleMouseOnClickNavBarBackground}
+      ></div>
+      <HeaderPageComponent
+        setNavBackgroundVisibility={setNavBackgroundVisibility}
+        navBackgroundVisibility={navBackgroundVisibility}
+        navBarVisibility={navBarVisibility}
+        setNavBarVisibility={setNavBarVisibility}
+      />
+      <InfoPageComponent />
+      <FooterPageComponent />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/HeaderPageComponent/HeaderPageComponent.jsx
+++ src/components/HeaderPageComponent/HeaderPageComponent.jsx
@@ -2,7 +2,6 @@ import "./HeaderPageComponent.css";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useRef } from "react";
-// import { useResize } from "react-spring/web";
 
 import logo from "../../assets/logo.svg";
 import viber from "../../assets/viber.svg";
@@ -10,15 +9,34 @@ import telephone from "../../assets/telephone.svg";
 import mail from "../../assets/mail.svg";
 import useResize from "../hooks/useResize";
 
-function HeaderPageComponent() {
-  const [navigationHover, setNavigationHover] = useState("");
+function HeaderPageComponent({
+  setNavBackgroundVisibility,
+  navBackgroundVisibility,
+  navBarVisibility,
+  setNavBarVisibility,
+}) {
   const [dropdownVisibility, setDropdownVisibility] = useState(false);
+  // const [navBarVisibility, setNavBarVisibility] = useState(false);
+
   const size = useResize();
 
   const headerDropdownRef = useRef();
   const dropdownContentRef = useRef();
+  const navigationBarMobileRef = useRef();
 
-  useEffect(() => {}, [navigationHover, setNavigationHover]);
+  useEffect(() => {
+    if (size[0] >= 800 && navBarVisibility) {
+      setNavBarVisibility(false);
+      setNavBackgroundVisibility(false);
+    }
+
+    if (!navBarVisibility && size[0] < 800) {
+      console.log(navBarVisibility + "  handleMouseOnClickNavBarButton");
+      navigationBarMobileRef.current.style.display = "none";
+    }
+  }, [size[0], navBarVisibility]);
+
+  // useEffect(() => {}), [navBarVisibility];
 
   const handleMouseEnter = () => {
     headerDropdownRef.current.style.overflow = "visible";
@@ -28,125 +46,196 @@ function HeaderPageComponent() {
     headerDropdownRef.current.style.overflow = "hidden";
   };
 
-  const handleMouseOnClick = () => {
+  const handleMouseOnClickNavBarButton = () => {
+    setNavBarVisibility(() => !navBarVisibility);
+    setNavBackgroundVisibility(!navBackgroundVisibility);
+
+    if (navBarVisibility) {
+      console.log(navBarVisibility + "  handleMouseOnClickNavBarButton");
+      navigationBarMobileRef.current.style.display = "none";
+    } else {
+      console.log(navBarVisibility + " handleMouseOnClickNavBarButton");
+      navigationBarMobileRef.current.style.display = "flex";
+    }
+  };
+
+  const handleMouseOnClickNavBar = () => {
     setDropdownVisibility(() => !dropdownVisibility);
 
     if (dropdownVisibility) {
       console.log(dropdownVisibility);
-      dropdownContentRef.current.style.display = "flex";
+      dropdownContentRef.current.style.display = "none";
     } else {
       console.log(dropdownVisibility);
-      dropdownContentRef.current.style.display = "none";
+      dropdownContentRef.current.style.display = "flex";
     }
   };
 
   return (
-    <div className="header__container">
-      <div className="left-side__container">
-        <div className="logo__container">
-          <img src={logo} alt="logo" />
-        </div>
-      </div>
-      <div className="right-side__container">
-        {size[0] < "800" ? (
-          <div className="navigation__container">
-            <nav className="header__panel__nav">
-              <button className="header__button">
-                <p>Головна</p>
-              </button>
-              <button className="header__button">
-                <p>Продаж техніки</p>
-              </button>
-              <button
-                className="header__button dropdown__button"
-                onClick={handleMouseOnClick}
-              >
-                <div>
-                  Послуги
-                  <i className="arrow down"></i>
-                </div>
-              </button>
-
-              <div className="dropdown__content" ref={dropdownContentRef}>
-                <a href="#" className="dropdown__link">
-                  <p>Довідка рахунок</p>
-                </a>
-                <a href="#" className="dropdown__link">
-                  <p>Продаж Т/П</p>
-                </a>
-              </div>
-
-              <button className="header__button">
-                <p>Контакти</p>
-              </button>
-            </nav>
-          </div>
-        ) : (
+    <div className="header">
+      <div className="header__container">
+        {size[0] < "800" && (
           <>
-            <div className="contacts__container">
-              <div className="contacts__item__container">
-                <a href="mailto:mail.example.com" className="contacts__item">
-                  {/* <i className="fa-solid fa-envelope-open-text"></i> */}
-                  <img src={mail} alt="mail_logo" className="contacts_icon" />
-                  <p>mail.example.com</p>
-                </a>
-              </div>
-              <div className="contacts__item__container">
-                <a href="viber://chat?number=%2B4" className="contacts__item">
-                  {/* <i className=""></i> */}
-                  <img src={viber} alt="viber_logo" className="contacts_icon" />
-                  <p>Viber</p>
-                </a>
-              </div>
-              <div className="contacts__item__container">
-                <a href="tel:+380503333333" className="contacts__item">
-                  {/* <i className=""></i> */}
-                  <img src={telephone} alt="telephone_logo" />
-                  <p>+38(050) 333 33 33</p>
-                </a>
-              </div>
-            </div>
-            <div className="adress__container">
-              <p>
-                Наша адреса: Село Гавронщина, Київська область <br />
-                вулиця Шевченка 18 {size[0]} {size[1]}
-              </p>
-            </div>
-            <div className="navigation__container">
+            <button
+              className="header__panel__nav__button"
+              onClick={handleMouseOnClickNavBarButton}
+            >
+              <hr />
+              <hr />
+              <hr />
+            </button>
+            <div className="navigation__container" ref={navigationBarMobileRef}>
               <nav className="header__panel__nav">
-                <button className="header__button header__button-first">
+                <button className="header__button">
                   <p>Головна</p>
                 </button>
                 <button className="header__button">
                   <p>Продаж техніки</p>
                 </button>
-                <div
-                  className="header__dropdown"
-                  ref={headerDropdownRef}
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
+                <button
+                  className="header__button dropdown__button"
+                  onClick={handleMouseOnClickNavBar}
                 >
-                  <button className="header__button dropdown__button">
-                    <div>
-                      Послуги<i className="arrow down"></i>
-                    </div>
-                  </button>
-                  <div className="dropdown__content">
-                    <a href="#" className="dropdown__link">
-                      <p>Довідка рахунок</p>
-                    </a>
-                    <a href="#" className="dropdown__link">
-                      <p>Продаж Т/П</p>
-                    </a>
+                  <div>
+                    Послуги
+                    <i className="arrow down"></i>
                   </div>
+                </button>
+
+                <div className="dropdown__content" ref={dropdownContentRef}>
+                  <a href="#" className="dropdown__link">
+                    <p>Довідка рахунок</p>
+                  </a>
+                  <a href="#" className="dropdown__link">
+                    <p>Продаж Т/П</p>
+                  </a>
                 </div>
-                <button className="header__button header__button-last">
+
+                <button className="header__button">
                   <p>Контакти</p>
                 </button>
               </nav>
             </div>
+            {/* <div className="navigation__background"></div> */}
           </>
         )}
+        <div className="left-side__container">
+          <div className="logo__container">
+            <img src={logo} alt="logo" />
+          </div>
+        </div>
+        <div className="right-side__container">
+          {size[0] < "800" ? (
+            <>
+              {/* <div className="header__panel__nav__button">
+              <br />
+              <br />
+              <br />
+            </div> */}
+              {/* <div className="navigation__container" ref={navigationBarMobileRef}>
+              <nav className="header__panel__nav">
+                <button className="header__button">
+                  <p>Головна</p>
+                </button>
+                <button className="header__button">
+                  <p>Продаж техніки</p>
+                </button>
+                <button
+                  className="header__button dropdown__button"
+                  onClick={handleMouseOnClickNavBar}
+                >
+                  <div>
+                    Послуги
+                    <i className="arrow down"></i>
+                  </div>
+                </button>
+
+                <div className="dropdown__content" ref={dropdownContentRef}>
+                  <a href="#" className="dropdown__link">
+                    <p>Довідка рахунок</p>
+                  </a>
+                  <a href="#" className="dropdown__link">
+                    <p>Продаж Т/П</p>
+                  </a>
+                </div>
+
+                <button className="header__button">
+                  <p>Контакти</p>
+                </button>
+              </nav>
+            </div> */}
+            </>
+          ) : (
+            <>
+              <div className="contacts__container">
+                <div className="contacts__item__container">
+                  <a href="mailto:mail.example.com" className="contacts__item">
+                    {/* <i className="fa-solid fa-envelope-open-text"></i> */}
+                    <img src={mail} alt="mail_logo" className="contacts_icon" />
+                    <p>mail.example.com</p>
+                  </a>
+                </div>
+                <div className="contacts__item__container">
+                  <a href="viber://chat?number=%2B4" className="contacts__item">
+                    {/* <i className=""></i> */}
+                    <img
+                      src={viber}
+                      alt="viber_logo"
+                      className="contacts_icon"
+                    />
+                    <p>Viber</p>
+                  </a>
+                </div>
+                <div className="contacts__item__container">
+                  <a href="tel:+380503333333" className="contacts__item">
+                    {/* <i className=""></i> */}
+                    <img src={telephone} alt="telephone_logo" />
+                    <p>+38(050) 333 33 33</p>
+                  </a>
+                </div>
+              </div>
+              <div className="adress__container">
+                <p>
+                  Наша адреса: Село Гавронщина, Київська область <br />
+                  вулиця Шевченка 18 {size[0]} {size[1]}
+                </p>
+              </div>
+              <div className="navigation__container">
+                <nav className="header__panel__nav">
+                  <button className="header__button header__button-first">
+                    <p>Головна</p>
+                  </button>
+                  <button className="header__button">
+                    <p>Продаж техніки</p>
+                  </button>
+                  <div
+                    className="header__dropdown"
+                    ref={headerDropdownRef}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    <button className="header__button dropdown__button">
+                      <div>
+                        Послуги<i className="arrow down"></i>
+                      </div>
+                    </button>
+                    <div className="dropdown__content">
+                      <a href="#" className="dropdown__link">
+                        <p>Довідка рахунок</p>
+                      </a>
+                      <a href="#" className="dropdown__link">
+                        <p>Продаж Т/П</p>
+                      </a>
+                    </div>
+                  </div>
+                  <button className="header__button header__button-last">
+                    <p>Контакти</p>
+                  </button>
+                </nav>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
